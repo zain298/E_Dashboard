7f5908240dc59d5661dba5cef24ce9d9ed95e65e
Ubaid Mehmood <ubaidwarraich001@gmail.com>
2021-07-07 14:12:02 +0500

    fixed all components

--- electionmanagement/src/app/politicalpartycandidateinformation/politicalpartycandidateinformation.component.ts
+++ electionmanagement/src/app/politicalpartycandidateinformation/politicalpartycandidateinformation.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { OnFailService } from "../services/on-fail.service";
 import { PoliticalpartycandidateinformationService } from "../politicalpartycandidateinformation/politicalpartycandidateinformation.service";
+import { PersoninformationService } from "../personinformation/personinformation.service";
 import { ToastrService } from "ngx-toastr";
+import { ElectioncontituencyinformationService } from "../electioncontituencyinformation/electioncontituencyinformation.service";
+import { PoliticalpartyinformationService } from "../politicalpartyinformation/politicalpartyinformation.service";
+
 import { LookupService } from "../lookup/lookup.service";
 
 declare var $: any;
@@ -14,20 +18,24 @@ declare var $: any;
 export class PoliticalpartycandidateinformationComponent implements OnInit {
   entitylist = [];
   politicalpartycandidateinformationAll = [];
+  personinformationAll=[];
+  contituencyinformationAll=[];
+  politicalpartyinformationAll=[];
   electiontypeActive = [];
   politicalpartycandidateinformation = {
     candidate_ID: 0,
-    forenames: "",
-    surname: "",
-    description: "", //province
+    // forenames: "",
+    // surname: "",
+    person_name:"",
+    // description: "", //province
     // electiontype_ID: {},
     politicalparty_NAME: "",
-    contituency_DESC: "",
+    // contituency_DESC: "",
     contituency_CODE: "",
-    politicalpartyleader_NAME: "",
-    politicalpartyleader_DESINATION: "",
-    politicalparty_SYMBOL: "",
-    politicalparty_ADDRESS: "",
+    // politicalpartyleader_NAME: "",
+    // politicalpartyleader_DESINATION: "",
+    // politicalparty_SYMBOL: "",
+    // politicalparty_ADDRESS: "",
     //description: "", //assembly
     contituency_ID: 0,
     person_ID: 0,
@@ -38,6 +46,10 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
   constructor(
     private politicalpartycandidateinformationservice: PoliticalpartycandidateinformationService,
     private onfailservice: OnFailService,
+    private personinformationservice: PersoninformationService,
+    private electioncontituencyinformationservice: ElectioncontituencyinformationService,
+    private politicalpartyinformationservice: PoliticalpartyinformationService,
+
     private toastrservice: ToastrService,
     private lookupservice: LookupService
   ) {}
@@ -61,45 +73,95 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
   }
 
   AddNew() {
-    // this.politicalpartycandidateinformation = {
-    //   candidate_ID: 0,
-    //   description: "",
-    //   surname: "",
-    //   forenames: "",
-    //   //  electiontype_ID: {},
-    //   politicalparty_NAME: "",
-    //   contituency: "",
-    //   contituencyCode: "",
-    //   assembly: "",
-    //   isactive: true,
-    // };
-    // this.getelectionType();
+    this.politicalpartycandidateinformation = {
+      candidate_ID: 0,
+      person_name:"",
+      politicalparty_NAME: "",
+      contituency_CODE: "",
+      contituency_ID: 0,
+      person_ID: 0,
+      politicalparty_ID: 0,
+      isactive: true,
+    };
+    this.getAllPersons();
+    this.getAllContituencies();
+    this.getAllPoliticalParties();
     $("#addModal").modal("show");
   }
-
+  getAllContituencies() {
+    this.electioncontituencyinformationservice.getAll().subscribe(
+      (response) => {
+        if (response) {
+          console.log(response);
+          if (response.error && response.status) {
+            this.toastrservice.warning("Message", " " + response.message);
+          } else {
+            this.contituencyinformationAll = response;
+          }
+        }
+      },
+      (error) => {
+        this.onfailservice.onFail(error);
+      }
+    );
+  }
+  getAllPoliticalParties() {
+    this.politicalpartyinformationservice.getAll().subscribe(
+      (response) => {
+        if (response) {
+          console.log(response);
+          if (response.error && response.status) {
+            this.toastrservice.warning("Message", " " + response.message);
+          } else {
+            this.politicalpartyinformationAll = response;
+          }
+        }
+      },
+      (error) => {
+        this.onfailservice.onFail(error);
+      }
+    );
+  }
+  getAllPersons() {
+    this.personinformationservice.getAll().subscribe(
+      (response) => {
+        if (response) {
+          console.log(response);
+          if (response.error && response.status) {
+            this.toastrservice.warning("Message", " " + response.message);
+          } else {
+            this.personinformationAll = response;
+          }
+        }
+      },
+      (error) => {
+        this.onfailservice.onFail(error);
+      }
+    );
+  }
   uploadorder() {
     $("#addModal").modal("show");
   }
 
   Edit(row) {
-    // this.politicalpartycandidateinformation = {
-    //   candidate_ID: row.data.candidate_ID,
-    //   description: row.data.description,
-    //   surname: row.data.surname,
-    //   forenames: row.data.forenames,
-    //   // electiontype_ID: row.data.electiontype_ID!=null?row.data.electiontype_ID.id:null,
-    //   politicalparty_NAME: row.data.politicalparty_NAME,
-    //   contituency: row.data.contituency,
-    //   contituencyCode: row.data.contituencyCode,
-    //   assembly: row.data.assembly,
-    //   isactive: true,
-    // };
+    this.politicalpartycandidateinformation = {
+      candidate_ID: row.data.candidate_ID,
+      person_name:row.data.person_ID.forenames+" "+row.data.person_ID.surname,
+      politicalparty_NAME: row.data.politicalparty_ID.politicalparty_NAME,
+      contituency_CODE: row.data.contituency_ID.contituency_CODE,
+      contituency_ID: row.data.contituency_ID.contituency_ID,
+      person_ID: row.data.person_ID.person_ID,
+      politicalparty_ID: row.data.politicalparty_ID.politicalparty_ID,
+      isactive: true,
+    };
     if (row.data.isactive == "Y") {
       this.politicalpartycandidateinformation.isactive = true;
     } else {
       this.politicalpartycandidateinformation.isactive = false;
     }
-    // this.getelectionType();
+    this.getAllPersons();
+    this.getAllContituencies();
+    this.getAllPoliticalParties();
     $("#editModal").modal("show");
   }
 
@@ -124,11 +186,54 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
   }
 
   add(politicalpartycandidateinformation) {
-    if (politicalpartycandidateinformation.electiontype_ID != null) {
-      politicalpartycandidateinformation.electiontype_ID =
-        politicalpartycandidateinformation.electiontype_ID.id;
+    if (
+      politicalpartycandidateinformation.contituency_CODE == ""
+    ) {
+      politicalpartycandidateinformation.contituency_ID =
+        this.contituencyinformationAll[0].contituency_ID;
     } else {
-      politicalpartycandidateinformation.electiontype_ID == null;
+      for (let contituency in this.contituencyinformationAll) {
+        if (
+          politicalpartycandidateinformation.contituency_CODE ==
+          this.contituencyinformationAll[contituency].contituency_CODE
+        ) {
+          politicalpartycandidateinformation.contituency_ID =
+          this.contituencyinformationAll[contituency].contituency_ID;
+        }
+      }
+    }
+    if (
+      politicalpartycandidateinformation.person_name == ""
+    ) {
+      politicalpartycandidateinformation.person_ID =
+        this.personinformationAll[0].person_ID;
+    } else {
+      for (let person in this.personinformationAll) {
+        if (
+          politicalpartycandidateinformation.person_name ==
+          this.personinformationAll[person].forenames+this.personinformationAll[person].surname
+        ) {
+          politicalpartycandidateinformation.person_ID =
+          this.personinformationAll[person].person_ID;
+        }
+      }
+    }
+    if (
+      politicalpartycandidateinformation.politicalparty_NAME == ""
+    ) {
+      politicalpartycandidateinformation.politicalparty_ID =
+        this.politicalpartyinformationAll[0].politicalparty_ID;
+    } else {
+      for (let party in this.politicalpartyinformationAll) {
+        if (
+          politicalpartycandidateinformation.politicalparty_NAME ==
+          this.politicalpartyinformationAll[party].politicalparty_NAME
+        ) {
+          politicalpartycandidateinformation.politicalparty_ID =
+          this.politicalpartyinformationAll[party].politicalparty_ID;
+  
+        }
+      }
     }
     this.politicalpartycandidateinformationservice
       .add(politicalpartycandidateinformation)
@@ -142,7 +247,7 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
             } else if (response.candidate_ID) {
               this.toastrservice.success(
                 "Success",
-                "New election Information Added"
+                "New Candidate Information Added"
               );
               this.politicalpartycandidateinformation = response;
               this.getAll();
@@ -169,7 +274,7 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
             } else if (response.candidate_ID) {
               this.toastrservice.success(
                 "Success",
-                "election Information Updated"
+                "Candidate Information Deleted"
               );
               this.politicalpartycandidateinformation = response;
               this.getAll();
@@ -185,6 +290,57 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
       );
   }
   update(politicalpartycandidateinformation) {
+    if (
+      politicalpartycandidateinformation.contituency_CODE == ""
+    ) {
+      politicalpartycandidateinformation.contituency_ID =
+        this.contituencyinformationAll[0].contituency_ID;
+    } else {
+      for (let contituency in this.contituencyinformationAll) {
+        if (
+          politicalpartycandidateinformation.contituency_CODE ==
+          this.contituencyinformationAll[contituency].contituency_CODE
+        ) {
+          politicalpartycandidateinformation.contituency_ID =
+          this.contituencyinformationAll[contituency].contituency_ID;
+        }
+      }
+    }
+    let personName=politicalpartycandidateinformation.person_name.split(" ");
+    let completeName=personName[0]+personName[1];
+    if (
+      politicalpartycandidateinformation.person_name == ""
+    ) {
+      politicalpartycandidateinformation.person_ID =
+        this.personinformationAll[0].person_ID;
+    } else {
+      for (let person in this.personinformationAll) {
+        if (
+         completeName ==
+          this.personinformationAll[person].forenames+this.personinformationAll[person].surname
+        ) {
+          politicalpartycandidateinformation.person_ID =
+          this.personinformationAll[person].person_ID;
+        }
+      }
+    }
+    if (
+      politicalpartycandidateinformation.politicalparty_NAME == ""
+    ) {
+      politicalpartycandidateinformation.politicalparty_ID =
+        this.politicalpartyinformationAll[0].politicalparty_ID;
+    } else {
+      for (let party in this.politicalpartyinformationAll) {
+        if (
+          politicalpartycandidateinformation.politicalparty_NAME ==
+          this.politicalpartyinformationAll[party].politicalparty_NAME
+        ) {
+          politicalpartycandidateinformation.politicalparty_ID =
+          this.politicalpartyinformationAll[party].politicalparty_ID;
+  
+        }
+      }
+    }
     if (politicalpartycandidateinformation.isactive == true) {
       politicalpartycandidateinformation.isactive = "Y";
     } else {
@@ -209,7 +365,7 @@ export class PoliticalpartycandidateinformationComponent implements OnInit {
             } else if (response.candidate_ID) {
               this.toastrservice.success(
                 "Success",
-                "election Information Updated"
+                "Candidate Information Updated"
               );
               this.politicalpartycandidateinformation = response;
               this.getAll();
